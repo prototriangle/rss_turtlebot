455606ba89d102dfd7b5bc3e435a41cab8af0d68
Alex Roy <prototriangle@gmail.com>
2019-11-18 21:53:43 +0000

    Initialise map metadata vectors in subscriber callback

--- src/rss_localization/src/MapHandler.cpp
+++ src/rss_localization/src/MapHandler.cpp
@@ -6,8 +6,18 @@ using namespace nav_msgs;
 namespace rss {
 
     void MapHandler::recCallback(const OccupancyGrid::ConstPtr &msg) {
-        currentMap.grid = *msg;
         currentMap.valid = true;
+        currentMap.grid = *msg;
+        for (unsigned long i = 0; i < currentMap.grid.data.size(); i++) {
+            if (currentMap.grid.data[i] < 50) { // probably empty
+                currentMap.freeSpaceIndices.push_back(i);
+            } else {
+                currentMap.occupiedSpaceIndices.push_back(i);
+            }
+        }
+        if (currentMap.freeSpaceIndices.empty()) {
+            ROS_WARN("No free space on map!");
+        }
     }
 
     Map MapHandler::currentMap;
